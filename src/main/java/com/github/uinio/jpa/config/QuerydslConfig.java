9448d1930c4e4c2f42108a3d8ad15db864e6527e
Jingle-Cat <895380034@qq.com>
2020-09-07 16:33:10 +0800

    Support Querydsl

---
+++ src/main/java/com/github/uinio/jpa/config/QuerydslConfig.java
@@ -0,0 +1,24 @@
+package com.github.uinio.jpa.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * QuerydslConfig
+ */
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
