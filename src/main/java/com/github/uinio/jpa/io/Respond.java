f37436a6481ac01fbba9430ba9f3bee6b50158aa
Jingle-Cat <895380034@qq.com>
2020-09-15 10:02:50 +0800

    fix serialization exception

--- src/main/java/com/github/uinio/jpa/io/Respond.java
+++ src/main/java/com/github/uinio/jpa/io/Respond.java
@@ -9,59 +9,127 @@ package com.github.uinio.jpa.io;
 
 public class Respond {
 
-    private final int status;
-
-    private final Object json;
-
-    private final String message;
-
-    private Respond(int status, String message, Object json) {
-        this.status = status;
-        this.json = json;
-        this.message = message;
+    private Respond() {
     }
+
+    private int status;
+
+    private Object json;
+
+    private String message;
 
     public static Respond respond() {
-        return new Respond(200, null, null);
+        final Respond respond = new Respond();
+        respond.setStatus(200);
+        return respond;
     }
 
-    public static Respond respond(int status, Object data) {
-        return new Respond(status, null, data);
+    public static Respond respond(int status, String message) {
+        final Respond respond = new Respond();
+        respond.setStatus(status);
+        respond.setMessage(message);
+        return respond;
     }
 
     public static Respond respond(int status, String message, Object data) {
-        return new Respond(status, message, data);
+        final Respond respond = new Respond();
+        respond.setStatus(status);
+        respond.setMessage(message);
+        respond.setJson(data);
+        return respond;
     }
 
     //200
     public static Respond ok(String message) {
-        return new Respond(200, message, null);
+        Respond respond = new Respond();
+        respond.setStatus(200);
+        respond.setMessage(message);
+        return respond;
     }
 
     //401
     public static Respond unAuthorized(String message) {
-        return new Respond(401, message, null);
+        Respond respond = new Respond();
+        respond.setStatus(401);
+        respond.setMessage(message);
+        return respond;
     }
 
     //403
     public static Respond forbidden(String message) {
-        return new Respond(403, message, null);
+        Respond respond = new Respond();
+        respond.setStatus(403);
+        respond.setMessage(message);
+        return respond;
+    }
+
+    //404
+    public static Respond notFound(String message) {
+        Respond respond = new Respond();
+        respond.setStatus(404);
+        respond.setMessage(message);
+        return respond;
+    }
+
+    //500
+    public static Respond error(String message) {
+        Respond respond = new Respond();
+        respond.setStatus(500);
+        respond.setMessage(message);
+        return respond;
     }
 
     public static Respond success(Object data) {
-        return new Respond(200, null, data);
+        final Respond respond = new Respond();
+        respond.setStatus(200);
+        respond.setJson(data);
+        return respond;
     }
 
     public static Respond success(String message, Object data) {
-        return new Respond(200, message, data);
+        final Respond respond = new Respond();
+        respond.setStatus(200);
+        respond.setMessage(message);
+        respond.setJson(data);
+        return respond;
     }
 
     public static Respond failure(String message) {
-        return new Respond(500, message, null);
+        final Respond respond = new Respond();
+        respond.setStatus(500);
+        respond.setMessage(message);
+        return respond;
     }
 
     public static Respond failure(String message, Object data) {
-        return new Respond(500, message, data);
+        final Respond respond = new Respond();
+        respond.setStatus(500);
+        respond.setMessage(message);
+        respond.setJson(data);
+        return respond;
     }
 
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Object getJson() {
+        return json;
+    }
+
+    public void setJson(Object json) {
+        this.json = json;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
